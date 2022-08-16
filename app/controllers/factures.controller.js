f48d29e2ff4aac2f65c3f83d2c8eb4f2b0b53970
aridhiseif <aridhi.seif@gmail.com>
2022-08-16 19:45:03 +0100

    making some changes to Schema

--- app/controllers/factures.controller.js
+++ app/controllers/factures.controller.js
@@ -2,145 +2,134 @@ const Facture = require("../models/factures.model");
 
 // Create and Save a new facture
 exports.create = (req, res) => {
-    // Validate request
-    if (!req.body.matricule) {
-        return res.status(400).send({
-            message: "facture content can not be empty",
-        });
-    }
-
-    // Create a facture
-    const facture = new Facture({
-        matricule: req.body.matricule,
-        prix: req.body.prix,
-        dates: req.body.dates,
-        datef: req.body.datef,
-        tel: req.body.tel,
+  // Validate request
+  if (!req.body.fullName) {
+    return res.status(400).send({
+      message: "facture content can not be empty",
     });
+  }
 
-    // Save facture in the database
-    facture
-        .save()
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            res.status(500).send({
-                message:
-                    err.message ||
-                    "Some error occurred while creating the facture.",
-            });
-        });
+  // Create a facture
+  const facture = new Facture({
+    fullName: req.body.fullName,
+    montant: req.body.montant,
+    dates: req.body.dates,
+    datef: req.body.datef,
+    tel: req.body.tel,
+  });
+
+  // Save facture in the database
+  facture
+    .save()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the facture.",
+      });
+    });
 };
 
 // Retrieve and return all facture from the database.
 exports.findAll = (req, res) => {
-    Facture.find()
-        .then((factures) => {
-            res.send(factures);
-        })
-        .catch((err) => {
-            res.status(500).send({
-                message:
-                    err.message ||
-                    "Some error occurred while retrieving factures.",
-            });
-        });
+  Facture.find()
+    .then((factures) => {
+      res.send(factures);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving factures.",
+      });
+    });
 };
 
 // Find a single facture with a factureId
 exports.findOne = (req, res) => {
-    Facture.findById(req.params.factureId)
-        .then((facture) => {
-            if (!facture) {
-                return res.status(404).send({
-                    message:
-                        "facture not found with id " + req.params.factureId,
-                });
-            }
-            res.send(facture);
-        })
-        .catch((err) => {
-            if (err.kind === "ObjectId") {
-                return res.status(404).send({
-                    message:
-                        "facture not found with id " + req.params.factureId,
-                });
-            }
-            return res.status(500).send({
-                message:
-                    "Error retrieving facture with id " + req.params.factureId,
-            });
+  Facture.findById(req.params.factureId)
+    .then((facture) => {
+      if (!facture) {
+        return res.status(404).send({
+          message: "facture not found with id " + req.params.factureId,
         });
+      }
+      res.send(facture);
+    })
+    .catch((err) => {
+      if (err.kind === "ObjectId") {
+        return res.status(404).send({
+          message: "facture not found with id " + req.params.factureId,
+        });
+      }
+      return res.status(500).send({
+        message: "Error retrieving facture with id " + req.params.factureId,
+      });
+    });
 };
 // login with credantials
 
 // Update a facture identified by the factureId in the request
 exports.update = (req, res) => {
-    // Validate Request
-    if (!req.body.firstname) {
-        return res.status(400).send({
-            message: "facture content can not be empty",
-        });
-    }
+  // Validate Request
+  if (!req.body.fullName) {
+    return res.status(400).send({
+      message: "facture content can not be empty",
+    });
+  }
 
-    // Find facture and update it with the request body
-    Facture.findByIdAndUpdate(
-        req.params.factureId,
-        {
-            matricule: req.body.matricule,
-            prix: req.body.prix,
-            dates: req.body.dates,
-            datef: req.body.datef,
-            tel: req.body.tel,
-        },
-        { new: true }
-    )
-        .then((facture) => {
-            if (!facture) {
-                return res.status(404).send({
-                    message:
-                        "facture not found with id " + req.params.factureId,
-                });
-            }
-            res.send(facture);
-        })
-        .catch((err) => {
-            if (err.kind === "ObjectId") {
-                return res.status(404).send({
-                    message:
-                        "facture not found with id " + req.params.factureId,
-                });
-            }
-            return res.status(500).send({
-                message:
-                    "Error updating facture with id " + req.params.factureId,
-            });
+  // Find facture and update it with the request body
+  Facture.findByIdAndUpdate(
+    req.params.factureId,
+    {
+      matricule: req.body.matricule,
+      montant: req.body.montant,
+      dates: req.body.dates,
+      datef: req.body.datef,
+      tel: req.body.tel,
+    },
+    { new: true }
+  )
+    .then((facture) => {
+      if (!facture) {
+        return res.status(404).send({
+          message: "facture not found with id " + req.params.factureId,
         });
+      }
+      res.send(facture);
+    })
+    .catch((err) => {
+      if (err.kind === "ObjectId") {
+        return res.status(404).send({
+          message: "facture not found with id " + req.params.factureId,
+        });
+      }
+      return res.status(500).send({
+        message: "Error updating facture with id " + req.params.factureId,
+      });
+    });
 };
 
 // Delete a facture with the specified factureId in the request
 exports.delete = (req, res) => {
-    Facture.findByIdAndRemove(req.params.factureId)
-        .then((facture) => {
-            if (!facture) {
-                return res.status(404).send({
-                    message:
-                        "facture not found with id " + req.params.factureId,
-                });
-            }
-            res.send({ message: "facture deleted successfully!" });
-        })
-        .catch((err) => {
-            if (err.kind === "ObjectId" || err.name === "NotFound") {
-                return res.status(404).send({
-                    message:
-                        "facture not found with id " + req.params.factureId,
-                });
-            }
-            return res.status(500).send({
-                message:
-                    "Could not delete facture with id " + req.params.factureId,
-            });
+  Facture.findByIdAndRemove(req.params.factureId)
+    .then((facture) => {
+      if (!facture) {
+        return res.status(404).send({
+          message: "facture not found with id " + req.params.factureId,
         });
+      }
+      res.send({ message: "facture deleted successfully!" });
+    })
+    .catch((err) => {
+      if (err.kind === "ObjectId" || err.name === "NotFound") {
+        return res.status(404).send({
+          message: "facture not found with id " + req.params.factureId,
+        });
+      }
+      return res.status(500).send({
+        message: "Could not delete facture with id " + req.params.factureId,
+      });
+    });
 };
--- app/models/factures.model.js
+++ app/models/factures.model.js
@@ -1,31 +1,43 @@
 const mongoose = require("mongoose");
+const moment = require("moment");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const FactureSchema = mongoose.Schema(
-    {
-        matricule: {
-            type: String,
-            required: true,
-        },
-        prix: {
-            type: String,
-            required: true,
-        },
-        dates: {
-            type: String,
-            required: true,
-        },
-        datef: {
-            type: String,
-            required: true,
-        },
-        tel: {
-            type: String,
-            required: true,
-        },
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    montant: {
+      type: String,
+      required: true,
+    },
+    dates: {
+      type: Date,
+      required: true,
+    },
+    datef: {
+      type: Date,
+      required: true,
+    },
+    tel: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
+
+FactureSchema.plugin(AutoIncrement, { inc_field: "facture" });
+
+FactureSchema.pre("save", function (next) {
+  console.log("Processing Dates...");
+  this.dates = moment(this.dates, "YYYY-MM-DD").format("MM-DD-YYYY");
+  this.datef = moment(this.datef, "YYYY-MM-DD").format("MM-DD-YYYY");
+  console.log("Processing Dates End");
+  next();
+});
 
 module.exports = mongoose.model("Facture", FactureSchema);
